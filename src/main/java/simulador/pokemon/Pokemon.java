92ec740b552346059059059c09f7e512e05839ce
Katherine Ortiz <katherinem.ortizb@konradlorenz.edu.co>
2024-05-16 10:37:45 -0500

    Adelanto chiquito

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -9,5 +9,12 @@ public abstract class Pokemon {
     
     public Pokemon(String nombre,int salud, int puntosAtaque,String tipo, String estado){
         this.nombre=nombre;
+        this.salud=salud;
+        this.puntosAtaque=puntosAtaque;
+        this.tipo=tipo;
+        this.estado=estado;
+        
     }
+    
+   public atacar
 }
