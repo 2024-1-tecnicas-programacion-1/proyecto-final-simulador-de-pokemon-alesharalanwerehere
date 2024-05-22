9d84f2b3178a32fe5e27c59c10d59c95881f64fe
SharWasHere <159039235+SharWasHere@users.noreply.github.com>
2024-05-21 19:51:24 -0500

    Update Batalla.java

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,5 +1,15 @@
-package simulador.batalla;
+package principal;
 
 public class Batalla {
-    // TODO: Aquí va tu código
+    private Pokemon pokemon1;
+    private Pokemon pokemon2;
+
+    public Batalla (Pokemon pokemon1, Pokemon pokemon2){
+        this.pokemon1 = pokemon1;
+        this.pokemon2 = pokemon2;
+    }
+    
+   public Pokemon iniciarBatalla(Pokemon pokemon1, Pokemon pokemon2){
+        return pokemon1;
+   }
 }
