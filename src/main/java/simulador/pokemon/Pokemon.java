c28813690b7eea14d810b041b9ca8f311547e723
AlanSchleider <142046830+AlanSchleider@users.noreply.github.com>
2024-05-26 22:01:19 -0500

    entrega

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -1,27 +1,57 @@
-package simulador.pokemon;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public abstract class Pokemon {
+// Clase Pokemon
+abstract class Pokemon {
     protected String nombre;
     protected int salud;
-    protected int puntosAtaque;
-    protected String tipo;
-    protected String estado;
-    
-    public Pokemon(String nombre,int salud, int puntosAtaque,String tipo, String estado){
-        this.nombre=nombre;
-        this.salud=salud;
-        this.puntosAtaque=puntosAtaque;
-        this.tipo=tipo;
-        this.estado=estado;
-        
-    }
-    
-    public abstract Pokemon atacar();
-    //no estoy segura del tipo de dato que esta recibiendo
-    
-    public abstract int recibirDaño();
-    
-    public abstract Pokemon entrenar();
-    //no estoy segura del tipo de dato que esta recibiendo
+    protected int puntosDeAtaque;
+    protected TipoPokemon tipo;
+    protected String estado = "Normal";
 
+    public Pokemon(String nombre, int salud, int puntosDeAtaque, TipoPokemon tipo) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public TipoPokemon getTipo() {
+        return tipo;
+    }
+
+    public void atacar(Pokemon oponente) {
+        double multiplicador = TipoPokemon.obtenerMultiplicadorDeDaño(this.tipo, oponente.getTipo());
+        int daño = (int) (this.puntosDeAtaque * multiplicador);
+        oponente.recibirDaño(daño);
+        System.out.println(this.nombre + " ataca a " + oponente.getNombre() + " causando " + daño + " puntos de daño.");
+    }
+
+    public void recibirDaño(int daño) {
+        this.salud -= daño;
+        if (this.salud <= 0) {
+            this.salud = 0;
+            this.estado = "Debilitado";
+        }
+    }
+
+    public void entrenar() {
+        this.puntosDeAtaque += 5;
+        this.salud += 5;
+        System.out.println(this.nombre + " ha sido entrenado. Ahora tu Salud: " + this.salud + ", Ataque: " + this.puntosDeAtaque);
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " [Salud: " + salud + ", Ataque: " + puntosDeAtaque + ", Tipo: " + tipo + ", Estado: " + estado + "]";
+    }
 }
